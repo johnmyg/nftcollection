18855f6fafc599c28106b362331b50375c0168f7
John Mayuga <johnmayuga@Johns-MacBook-Air.local>
2023-02-14 17:18:41 -0500

    Smart contract creation

---
+++ hardhat/constants/index.js
@@ -0,0 +1,5 @@
+const WHITELIST_CONTRACT_ADDRESS = "0x082Fcc01B3ca0f503Dca47975d2cCF790e618137";
+
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = {WHITELIST_CONTRACT_ADDRESS, METADATA_URL}
